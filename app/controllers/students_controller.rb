1f865fe0aed1d99ae68b30e4fd2c01e7122e348b
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-07-19 03:37:39 +0300

    commit tree cc5a1652766565523b47dceaf1cee798a6afaff5

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,12 @@
+class StudentsController < ApplicationController
+    def index
+        render json: Student.all
+    end 
+
+    def grades
+        render json: Student.order("grade DESC")
+        
+    end 
+
+   
+end
